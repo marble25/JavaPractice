165b376db98866f38a305a380b8b6ad01b26a602
user <user@sec>
2015-03-18 14:24:29 +0900

    두 번째 실습과제 제출

--- JavaPractice/src/org/dimigo/basic/Promotion.java
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -8,10 +8,12 @@ public class Promotion {
 		
 		long annual_income = (long)pay_month*person*shop*12;
 		
-		System.out.println("<< 디미베네 연간 인건비>>");
+		System.out.println("<< 디미베네 연간 인건비 >>");
 		System.out.println("월 평균 급여 : "+String.format("%,d", pay_month)+"원");
 		System.out.println("점포 내 직원 수 : "+person+"명");
 		System.out.println("점포 수 : "+String.format("%,d", shop)+"개");
+		System.out.println();
+		System.out.println();
 		System.out.println("연간 인건비 : "+String.format("%,d", annual_income)+"원");
 	}
 }
