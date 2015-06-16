ff50d17596bd71dc0891896bca42107234fa1652
user <user@sec>
2015-06-16 11:50:46 +0900

    12 실습과제

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_FamilyMember
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 6. 16.
+ * </pre>
+ *
+ * @author		: user
+ * @version		: 1.0
+ */
+public class FamilyMember {
+	private static int memberCnt;
+	private String memberName;
+	
+	public FamilyMember(String memberName)
+	{
+		this.memberName=memberName;
+		memberCnt++;
+	}
+	public String getMemberName()
+	{
+		return this.memberName;
+	}
+	public static void printMemberCnt()
+	{
+		System.out.println("가족 총 인원수 : "+memberCnt+"명");
+	}
+}
