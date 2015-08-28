1806c6e5c1898c44ff20be90db6a381bea415ad4
2326 이지호 <ljiho1998@naver.com>
2015-08-28 14:27:55 +0900

    실습과제

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_SmartPhoneTest
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 28.
+ * </pre>
+ *
+ * @author		: user
+ * @version		: 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {new IPhone("iPhone 6", "애플", 700000), 
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		
+		for(SmartPhone phone : phones){
+			System.out.println(phone.toString());
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+		}
+	}
+}
